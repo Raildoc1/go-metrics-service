44aaf1a7c2a55418ef7140dbfe0531b8c7d98ffc
Kirill Gaiduk <raildoc@Kirills-MacBook-Pro.local>
2024-12-16 19:06:07 +0300

    fix

--- internal/server/middleware/with-response-compression.go
+++ internal/server/middleware/with-response-compression.go
@@ -4,14 +4,7 @@ import (
 	"compress/gzip"
 	"io"
 	"net/http"
-	"slices"
 	"strings"
-)
-
-const (
-	contentApplicationJSON = "application/json"
-	contentTextPlain       = "text/plain"
-	contentHTML            = "text/html"
 )
 
 type gzipWriter struct {
@@ -25,18 +18,9 @@ func (w gzipWriter) Write(b []byte) (int, error) {
 }
 
 func WithResponseCompression(h http.Handler, logger Logger) http.Handler {
-	contentToCompress := []string{
-		contentTextPlain,
-		contentApplicationJSON,
-		contentHTML,
-	}
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
 			logger.Debugln(r.Method, " ", r.URL, " compression missed")
-			h.ServeHTTP(w, r)
-			return
-		}
-		if !slices.Contains(contentToCompress, r.Header.Get("Content-Type")) {
 			h.ServeHTTP(w, r)
 			return
 		}
