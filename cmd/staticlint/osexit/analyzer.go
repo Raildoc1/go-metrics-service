d143d2dfb4f1766b7109438e49db25bce2e4652f
Kirill Gaiduk <raildoc@Kirills-MacBook-Pro.local>
2025-04-05 14:07:15 +0300

    wip

--- cmd/staticlint/osexit/analyzer.go
+++ cmd/staticlint/osexit/analyzer.go
@@ -22,27 +22,33 @@ func run(pass *analysis.Pass) (interface{}, error) {
 		ast.Inspect(f, func(n ast.Node) bool {
 			switch n := n.(type) {
 			case *ast.FuncDecl:
-				if n.Name.Name != "main" {
-					return false
+				if n.Name.Name == "main" {
+					return true
 				}
 			case *ast.File:
-				if n.Name.Name != "main" {
-					return false
+				if n.Name.Name == "main" {
+					return true
 				}
+			case *ast.GenDecl:
+				return true
 			case *ast.ImportSpec:
 				if n.Path.Value == osPackageDecl && n.Name != nil {
 					currentOsPackageName = n.Name.Name
 				}
-			case *ast.CallExpr:
-				if s, ok := n.Fun.(*ast.SelectorExpr); ok {
-					if i, ok := s.X.(*ast.Ident); ok {
-						if i.Name == currentOsPackageName && s.Sel.Name == "Exit" {
-							pass.Reportf(i.NamePos, "os.Exit call from main")
+			case *ast.BlockStmt:
+				return true
+			case *ast.ExprStmt:
+				if call, ok := n.X.(*ast.CallExpr); ok {
+					if s, ok := call.Fun.(*ast.SelectorExpr); ok {
+						if i, ok := s.X.(*ast.Ident); ok {
+							if i.Name == currentOsPackageName && s.Sel.Name == "Exit" {
+								pass.Reportf(i.NamePos, "os.Exit call from main")
+							}
 						}
 					}
 				}
 			}
-			return true
+			return false
 		})
 	}
 	return nil, nil
